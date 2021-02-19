78c161c9f2b004d011d12d9f65e7974bffc0477c
남성룡 <nambak80@gmail.com>
2021-02-19 21:25:08 +0900

    메인 페이지 제목 수정

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>패션 쇼핑몰 가격 비교 검색</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
