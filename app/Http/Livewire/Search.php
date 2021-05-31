5b969dca93d7ede17178afd742b46d0abb27cf89
남성룡 <nambak80@gmail.com>
2021-05-31 19:30:04 +0900

    기본 검색 기능 삭제

--- app/Http/Livewire/Search.php
+++ app/Http/Livewire/Search.php
@@ -3,13 +3,12 @@
 namespace App\Http\Livewire;
 
 use App\Crawler\Crawler;
-use App\Crawler\MusinsaRankingKeywordCrawler;
 use Livewire\Component;
 
 class Search extends Component
 {
-    public $keyword;
-    public $result;
+    public    $keyword;
+    public    $result;
     protected $queryString = ['keyword'];
 
     protected $rules = [
@@ -18,13 +17,6 @@ class Search extends Component
 
     public function render()
     {
-        if (is_null($this->keyword)) {
-            $crawler = new MusinsaRankingKeywordCrawler();
-            $this->keyword = $crawler->getKeyword();
-
-            $this->submit();
-        }
-
         return view('livewire.search');
     }
 
