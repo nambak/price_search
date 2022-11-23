75f6e102c1a2ea15c99b47e002b4b86928738344
10min.squad <32353193+market07@users.noreply.github.com>
2022-11-23 15:35:25 +0900

    스타일쉐어 크롤러 삭제(스타일쉐어 서비스 종료)

--- app/Crawler/Crawler.php
+++ app/Crawler/Crawler.php
@@ -9,7 +9,6 @@ class Crawler
     private $musinsa;
     private $brandi;
     private $seoulStore;
-    private $styleShare;
     private $result;
 
     public function __construct()
@@ -18,7 +17,6 @@ class Crawler
         $this->musinsa = new MusinsaCrawler();
         $this->brandi = new BrandiCrawler();
         $this->seoulStore = new SeoulStoreCrawler();
-        $this->styleShare = new StyleShareCrawler();
         $this->store29cm = new Store29cmCrawler();
     }
 
@@ -27,7 +25,6 @@ class Crawler
         $this->result = $this->result->concat($this->musinsa->search($keyword));
         $this->result = $this->result->concat($this->brandi->search($keyword));
         $this->result = $this->result->concat($this->seoulStore->search($keyword));
-        $this->result = $this->result->concat($this->styleShare->search($keyword));
         $this->result = $this->result->concat($this->store29cm->search($keyword));
 
         $sorted = $this->result->sortBy('price');
