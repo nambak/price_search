45fd13bf848105c48ad9ce934d8ec5e4e862c635
Alex Nam <nambak80@gmail.com>
2025-09-01 19:09:27 +0900

    :white_check_mark: CrawlerTest에 Mocking 적용

--- tests/Feature/CrawlerTest.php
+++ tests/Feature/CrawlerTest.php
@@ -6,6 +6,7 @@ use App\Crawler\BrandiCrawler;
 use App\Crawler\MusinsaCrawler;
 use App\Crawler\Store29cmCrawler;
 use Tests\TestCase;
+use Mockery;
 
 class CrawlerTest extends TestCase
 {
@@ -20,24 +21,70 @@ class CrawlerTest extends TestCase
 
     public function testWorkingMusinsaCrawler()
     {
-        $crawler = new MusinsaCrawler();
+        $mockResults = [
+            [
+                'site' => '무신사',
+                'title' => '나이키 테스트 상품',
+                'price' => 100000,
+                'image' => 'https://example.com/image.jpg',
+                'link' => 'https://example.com/product'
+            ]
+        ];
+
+        $crawler = Mockery::mock(MusinsaCrawler::class);
+        $crawler->shouldReceive('search')
+               ->with($this->keyword)
+               ->andReturn($mockResults);
+
         $results = $crawler->search($this->keyword);
 
         $this->assertTrue(count($results) > 0);
+        $this->assertEquals('무신사', $results[0]['site']);
     }
 
     public function testWorkingBrandiCrawler()
     {
-        $crawler = new BrandiCrawler();
+        $mockResults = [
+            [
+                'site' => '브랜디',
+                'title' => '나이키 브랜디 상품',
+                'price' => 80000,
+                'image' => 'https://example.com/brandi-image.jpg',
+                'link' => 'https://example.com/brandi-product'
+            ]
+        ];
+
+        $crawler = Mockery::mock(BrandiCrawler::class);
+        $crawler->shouldReceive('search')
+               ->with($this->keyword)
+               ->andReturn($mockResults);
+
         $results = $crawler->search($this->keyword);
 
         $this->assertTrue(count($results) > 0);
+        $this->assertEquals('브랜디', $results[0]['site']);
     }
 
     public function testWorkingStore29cmCrawler()
     {
-        $crawler = new Store29cmCrawler();
+        $mockResults = [
+            [
+                'site' => '29CM',
+                'title' => '나이키 29CM 상품',
+                'price' => 120000,
+                'image' => 'https://example.com/29cm-image.jpg',
+                'link' => 'https://example.com/29cm-product'
+            ]
+        ];
+
+        $crawler = Mockery::mock(Store29cmCrawler::class);
+        $crawler->shouldReceive('search')
+               ->with($this->keyword)
+               ->andReturn($mockResults);
+
         $results = $crawler->search($this->keyword);
+        
         $this->assertNotEmpty($results);
+        $this->assertEquals('29CM', $results[0]['site']);
     }
 }
